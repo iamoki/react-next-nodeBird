5b69a1427b1a213e70e833c43f13662690d36674
iamoki <pureaoh@gmail.com>
2022-03-27 15:55:30 +0900

    미들웨어와 리덕스 데브툴즈설정

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,16 +1,21 @@
 // configureStore를 만드는 법
 //next-redux-wrapper를 설치해 createWrapper를 가져올 수 있다. 
 import { createWrapper } from 'next-redux-wrapper';
-import { createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducer from '../reducers';
 
 const configureStore = () => {
-    const store = createStore(reducer);
-    store.dispatch({
-        type: 'CHANGE_NICKNAME',
-        data: 'odokodok',
-    })
+    const middlewares = [];
+    const enhancer = process.env.NODE_ENV === 'production'
+        ? compose(applyMiddleware(...middlewares))
+        : composeWithDevTools(applyMiddleware(...middlewares))
+    const store = createStore(reducer, enhancer);
+    // store.dispatch({
+    //     type: 'CHANGE_NICKNAME',
+    //     data: 'odokodok',
+    // })
     return store;
 };
 
